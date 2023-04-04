3f14a9d43738826b65321f63fe1cd2b79169ee42
tckthecreator <dsoouuzavitu@gmail.com>
2023-04-03 23:00:01 -0300

    test

--- repositories/create_auth.go
+++ repositories/create_auth.go
@@ -3,26 +3,24 @@ package repositories
 import (
 	"database/sql"
 	"fmt"
+	"log"
 )
 
 func CreateAuthRepository(db *sql.DB, contact, passwordHash string) error {
-	stmt, err := db.Prepare(`INSERT INTO auths("contact", "password") VALUES ('$1', '$2')`)
+	result, err := db.Exec(`INSERT INTO auths("contact", "password") VALUES ('$1', '$2')`, contact, passwordHash)
 	if err != nil {
-		return err
-	}
-
-	defer stmt.Close()
-	result, err := stmt.Exec(contact, passwordHash)
-	if err != nil {
+		log.Println(2)
 		return err
 	}
 
 	rowsAff, err := result.RowsAffected()
 	if err != nil {
+		log.Println(3)
 		return err
 	}
 
 	if rowsAff != 1 {
+		log.Println(4)
 		return fmt.Errorf("Internal Server Error")
 	}
 
